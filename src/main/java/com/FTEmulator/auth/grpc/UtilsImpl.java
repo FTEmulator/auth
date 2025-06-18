3882c15a64f8e4a97d28a1289e9eebcd7fd9739e
alexwebdev05 <alexwebdev05@proton.me>
2025-06-18 11:31:34 +0200

    util status with gRPC

---
+++ src/main/java/com/FTEmulator/auth/grpc/UtilsImpl.java
@@ -0,0 +1,18 @@
+package com.FTEmulator.auth.grpc;
+
+import org.springframework.grpc.server.service.GrpcService;
+
+import com.FTEmulator.auth.grpc.UtilsOuterClass.AuthStatusResponse;
+
+import io.grpc.stub.StreamObserver;
+
+@GrpcService
+public class UtilsImpl extends UtilsGrpc.UtilsImplBase {
+        @Override
+        public void authStatus(UtilsOuterClass.AuthStatusRequest request, StreamObserver<AuthStatusResponse> responseObserver) {
+            UtilsOuterClass.AuthStatusResponse response = AuthStatusResponse.newBuilder().setOk(true).build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+}
